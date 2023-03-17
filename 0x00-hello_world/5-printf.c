17ccd20e97558ec198f5f56e87492ba684c55781
Mohammed <filalimeftah@gmail.com>
2023-03-16 19:32:24 -0700

    l

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main: This function is the entry point of the program.
+*printf: It prints out a statement
+*returns: always 0 (succes (success))
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
